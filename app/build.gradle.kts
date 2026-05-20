a99868c43dea1b4aeddf61da1c40f5f25e7f6ffd
Vasu Gupta <vasu.ragv2013@gmail.com>
2026-05-20 19:10:15 +0530

    Add androidx.compose.material3 dependency to fix compilation errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.tv:tv-foundation:1.0.0-alpha10")
     implementation("androidx.tv:tv-material:1.0.0-alpha10")
     implementation("androidx.activity:activity-compose:1.8.2")
